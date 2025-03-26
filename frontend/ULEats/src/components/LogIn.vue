1ad516611f0735f180332236cde74de414dde74d
calvam10 <calvam10@estudiantes.unileon.es>
2025-03-27 00:03:34 +0100

    chore:  login component snippet

---
+++ frontend/ULEats/src/components/LogIn.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-text-field :label="username" :v-model = "username_value"></v-text-field>
+    <v-text-field :label="password" :v-mode = "password_value"></v-text-field>
+
+    <v-btn>
+        Submit
+    </v-btn>
+
+</template>
+
+<script lang="ts" setup>
+    const username = "Username"
+    const password = "Password"
+
+    let username_value = ""
+    let password_value = ""
+
+</script>
