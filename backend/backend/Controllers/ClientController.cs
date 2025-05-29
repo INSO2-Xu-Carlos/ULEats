d73021db478e0336d3e84b293a294978839e57a5
calvam10 <calvam10@estudiantes.unileon.es>
2025-05-29 20:47:03 +0200

    chore: now client and restaurant has services

--- backend/backend/Controllers/ClientController.cs
+++ backend/backend/Controllers/ClientController.cs
@@ -1,4 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using backend.Model;
+using LinqToDB;
+using System.Linq;
+using Microsoft.AspNetCore.Identity.Data;
+using backend.Core;
+using System.Text.RegularExpressions;
 
 namespace backend.Controllers
 {
@@ -6,28 +12,73 @@ namespace backend.Controllers
     [Route("[controller]")]
     public class ClientController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
 
-        private readonly ILogger<ClientController> _logger;
+        private readonly ClientService _clientService;
+        //private readonly ILogger<ClientController> _logger;
 
-        public ClientController(ILogger<ClientController> logger)
+        public ClientController(ClientService clientService)
         {
-            _logger = logger;
+            _clientService = clientService;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            bool isValid = _clientService.login(request.Email, request.Password);
+            if (!isValid) {
+                return Unauthorized("Bad credentials"); 
+            }
+
+            return Ok("Login Succesful");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterRequest request) {
+            if (string.IsNullOrWhiteSpace(request.Email) ||
+                string.IsNullOrWhiteSpace(request.Password) ||
+                string.IsNullOrWhiteSpace(request.FirstName) ||
+                string.IsNullOrWhiteSpace(request.LastName) ||
+                string.IsNullOrWhiteSpace(request.UserType))
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return BadRequest("All filds must be filled!");
+            }
+
+            var emailPattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+            if (!Regex.IsMatch(request.Email, emailPattern))
+            {
+                return BadRequest("Email is not in a valid pattern");
+            }
+
+            if (request.Password.Length < 8)
+            {
+                return BadRequest("Password is weak, please enter more characters");
+            }
+            bool  success = _clientService.register(request.FirstName, request.Password, request.Email, request.LastName, request.Phone, request.UserType);
+            if (!success) {
+                return BadRequest("Email is already registered"); 
+            }
+
+            return Ok("Register Succesful!");
         }
+
+        public class LoginRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class RegisterRequest
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string FirstName { get; set; }
+            public string? Phone { get; set; }
+            public string UserType { get; set; }
+            public string LastName { get; set; }
+        }
+
+        
+       
     }
 }
---
+++ backend/backend/Core/RegisterService.cs
@@ -0,0 +1,52 @@
+﻿using DataModel;
+using LinqToDB;
+using LinqToDB.SqlQuery;
+
+namespace backend.Core
+{
+    public class RegisterService
+    {
+        private readonly UlEatsDb _context;
+
+        public RegisterService(UlEatsDb context) { 
+            
+            _context = context;
+        }
+
+
+        /// <summary>
+        /// Register a new restaurant
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="name"></param>
+        /// <param name="address"></param>
+        /// <param name="phone"></param>
+        /// <param name="description"></param>
+        /// <param name="logoUrl"></param>
+        /// <returns>true if </returns>
+        public bool RegisterRestaurant(int userId, string name, string address, string phone, string? description, string? logoUrl)
+        {
+            var existing = _context.GetTable<Restaurant>().FirstOrDefault(r => r.UserId == userId);
+
+            if (existing != null)
+            {
+                return false;
+            }
+
+            var newRestaurant = new Restaurant
+            {
+                Name = name,
+                Address = address,
+                Phone = phone,
+                UserId = userId,
+                Description = description,
+                LogoUrl = logoUrl
+            };
+            
+            _context.Insert(newRestaurant);
+
+
+            return true;
+        }
+    }
+}
