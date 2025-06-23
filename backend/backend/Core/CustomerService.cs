0686b24000fb2eb9e7b7ac329e42cf91efaff952
calvam10 <calvam10@estudiantes.unileon.es>
2025-06-23 17:33:14 +0200

    doc: documentated customer service

--- backend/backend/Core/CustomerService.cs
+++ backend/backend/Core/CustomerService.cs
@@ -12,6 +12,11 @@ namespace backend.Core
             _context = context;
         }
 
+        /// <summary>
+        /// Add a new customer to the database
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns> The customer added if added correctly </returns>
         public Customer? AddCustomer(CustomerCreateDTO dto)
         {
             var customer = new Customer
@@ -28,22 +33,42 @@ namespace backend.Core
             }
             return null;
         }
+
+        /// <summary>
+        /// Get a customer by their ID
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns> Customer with given id if any </returns>
         public Customer? GetCustomerById(int customerId)
         {
             return _context.Customers.FirstOrDefault(c => c.CustomerId == customerId);
         }
 
+        /// <summary>
+        /// Get all customers
+        /// </summary>
+        /// <returns> List with all customers </returns>
         public IEnumerable<Customer> GetAllCustomers()
         {
             return _context.Customers.ToList();
         }
 
+        /// <summary>
+        /// Update an existing customer in the database
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns> true if updated correctly </returns>
         public bool UpdateCustomer(Customer customer)
         {
             var updated = _context.Update(customer);
             return updated > 0;
         }
 
+        /// <summary>
+        /// Delete a customer by their ID
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns> true if its deleted correctly </returns>
         public bool DeleteCustomer(int customerId)
         {
             var deleted = _context.Customers.Delete(c => c.CustomerId == customerId);
