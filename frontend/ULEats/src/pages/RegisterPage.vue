da2ae63a5260caf31b404cb205125696dc682f19
xuchen02 <xuchen00@estudiantes.unileon.es>
2025-06-23 18:43:24 +0100

    fix

--- frontend/ULEats/src/pages/RegisterPage.vue
+++ frontend/ULEats/src/pages/RegisterPage.vue
@@ -94,7 +94,7 @@ export default {
             "Content-Type": "application/json",
           },
           body: JSON.stringify(payload),
-        });
+        }).text();
 
         if (!response.ok) {
           const errorText = await response.text();
