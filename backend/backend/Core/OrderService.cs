fc825056ce38926580af3cb1e2f8df2229d1f91c
calvam10 <calvam10@estudiantes.unileon.es>
2025-06-23 17:33:46 +0200

    doc: documentated order service

--- backend/backend/Core/OrderService.cs
+++ backend/backend/Core/OrderService.cs
@@ -15,6 +15,11 @@ namespace backend.Core
             _context = context;
         }
 
+        /// <summary>
+        /// Add a new Order to the database
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns> The order if added to database correctly </returns>
         public Order? AddOrder(OrderCreateDTO dto)
         {
             var order = new Order
@@ -42,16 +47,30 @@ namespace backend.Core
             return null;
         }
 
+        /// <summary>
+        /// Get an Order by its ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Order with given id if any </returns>
         public Order? GetOrderById(int id)
         {
             return _context.Orders.FirstOrDefault(o => o.OrderId == id);
         }
 
+        /// <summary>
+        /// Get all Orders
+        /// </summary>
+        /// <returns> List with all orders </returns>
         public IEnumerable<Order> GetAllOrders()
         {
             return _context.Orders.ToList();
         }
 
+        /// <summary>
+        /// Create a new Order in the database
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns> Order if created correctly</returns>
         public Order? CreateOrder(Order order)
         {
             var id = _context.InsertWithInt32Identity(order);
@@ -63,6 +82,12 @@ namespace backend.Core
             return null;
         }
 
+        /// <summary>
+        /// Update an existing Order in the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="dto"></param>
+        /// <returns> true if updated correctly </returns>
         public bool UpdateOrder(int id, OrderCreateDTO dto)
         {
             var affected = _context.Orders
@@ -71,18 +96,33 @@ namespace backend.Core
                 .Set(o => o.Status, dto.Status)
                 .Update();
             return affected > 0;
-        } 
+        }
 
+        /// <summary>
+        /// Get all Orders associated with a specific Delivery ID
+        /// </summary>
+        /// <param name="deliveryId"></param>
+        /// <returns> list with all orders from a delivery </returns>
         public IEnumerable<Order> GetOrdersByDeliveryId(int deliveryId)
         {
             return _context.Orders.Where(o => o.DeliveryId == deliveryId).ToList();
         }
 
+        /// <summary>
+        /// Get all Orders associated with a specific Customer ID
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns> list with all orders from a customer</returns>
         public IEnumerable<Order> GetOrdersByCustomerId(int customerId)
         {
             return _context.Orders.Where(o => o.CustomerId == customerId).ToList();
         }
 
+        /// <summary>
+        /// Delete an Order by its ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> true if deleted correctly </returns>
         public bool DeleteOrder(int id)
         {
             var affected = _context.Orders
@@ -91,6 +131,11 @@ namespace backend.Core
             return affected > 0;
         }
 
+        /// <summary>
+        /// Get all Orders associated with a specific Restaurant User ID
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns> list with all orders from a restaurant </returns>
         public List<object> GetOrdersByRestaurantUser(int userId)
         {
 
@@ -108,6 +153,13 @@ namespace backend.Core
 
             return orders;
         }
+        /// <summary>
+        /// Update the status and delivery ID of an Order
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="status"></param>
+        /// <param name="deliveryId"></param>
+        /// <returns> true if updated correctly </returns>
         public bool UpdateOrderStatusAndDelivery(int orderId, string status, int? deliveryId)
         {
             var affected = _context.Orders
